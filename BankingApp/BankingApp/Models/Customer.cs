ffead44fd1fdf69f424b1ede163abd051dfdc391
eswaribala <parameswaribala@gmail.com>
2023-03-29 15:46:06 +0530

    updated

--- BankingApp/BankingApp/Models/Customer.cs
+++ BankingApp/BankingApp/Models/Customer.cs
@@ -1,7 +1,32 @@
-﻿namespace BankingApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankingApp.Models
 {
     public class Customer
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        [Column("Customer_Id")]
+        public long CustomerId { get; set; }
+        [Column("First_Name")]
+        [Required]
+        [RegularExpression("^[a-zA-Z]{5,25}$",ErrorMessage ="First Name Should be in alphabets within the range of 5,25")]
+        public string? FirstName { get; set; }
+        [Column("Last_Name")]
+        [Required]
+        [RegularExpression("^[a-zA-Z]{5,25}$", ErrorMessage = "Last Name Should be in alphabets within the range of 5,25")]
+        public string? LastName { get; set; }
+        [Required]
+        public long ContactNo { get; set; }
+        [Column("Email")]
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Column("Password")]
+        [Required]
+        public string? Password { get; set; }
 
 
     }
