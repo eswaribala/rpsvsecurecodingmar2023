d653d05073140db7981e5b44eb653b6b42bcfd04
eswaribala <parameswaribala@gmail.com>
2023-03-29 18:10:08 +0530

    updated

---
+++ BankingApp/BankingApp/Migrations/20230329123723_MyFirstMigration.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankingApp.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Customer_Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    First_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Last_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactNo = table.Column<long>(type: "bigint", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Customer_Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Address_Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Door_No = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Street_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Pin_Code = table.Column<long>(type: "bigint", nullable: false),
+                    CustomerId_FK = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Address_Id);
+                    table.ForeignKey(
+                        name: "FK_Address_Customer_CustomerId_FK",
+                        column: x => x.CustomerId_FK,
+                        principalTable: "Customer",
+                        principalColumn: "Customer_Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Corporate",
+                columns: table => new
+                {
+                    Customer_Id = table.Column<long>(type: "bigint", nullable: false),
+                    CompanType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Corporate", x => x.Customer_Id);
+                    table.ForeignKey(
+                        name: "FK_Corporate_Customer_Customer_Id",
+                        column: x => x.Customer_Id,
+                        principalTable: "Customer",
+                        principalColumn: "Customer_Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Individual",
+                columns: table => new
+                {
+                    Customer_Id = table.Column<long>(type: "bigint", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    DOB = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Individual", x => x.Customer_Id);
+                    table.ForeignKey(
+                        name: "FK_Individual_Customer_Customer_Id",
+                        column: x => x.Customer_Id,
+                        principalTable: "Customer",
+                        principalColumn: "Customer_Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_CustomerId_FK",
+                table: "Address",
+                column: "CustomerId_FK");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropTable(
+                name: "Corporate");
+
+            migrationBuilder.DropTable(
+                name: "Individual");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+        }
+    }
+}
