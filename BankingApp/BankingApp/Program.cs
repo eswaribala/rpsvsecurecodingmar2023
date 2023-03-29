87c46f85b7ee59ede0f968e18b4fa787254d5b5a
eswaribala <parameswaribala@gmail.com>
2023-03-29 17:51:22 +0530

    updated

--- BankingApp/BankingApp/Program.cs
+++ BankingApp/BankingApp/Program.cs
@@ -1,7 +1,30 @@
+using BankingApp.Configurations;
+using Microsoft.Data.SqlClient;
 using Steeltoe.Extensions.Configuration.ConfigServer;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddConfigServer();
+ConfigurationManager configuration = builder.Configuration;
+
+Dictionary<String, Object> data = new VaultConfiguration(configuration)
+    .GetDBCredentials().Result;
+Console.WriteLine(data);
+SqlConnectionStringBuilder providerCs = new SqlConnectionStringBuilder();
+//reading from Vault server
+providerCs.InitialCatalog = data["vsecuredbname"].ToString();
+providerCs.UserID = data["username"].ToString();
+providerCs.Password = data["password"].ToString();
+//providerCs.DataSource = "DESKTOP-55AGI0I\\MSSQLEXPRESS2021";
+//reading via config server
+providerCs.DataSource = configuration["trainerservername"];
+
+//providerCs.UserID = CryptoService2.Decrypt(ConfigurationManager.
+//AppSettings["UserId"]);
+providerCs.MultipleActiveResultSets = true;
+providerCs.TrustServerCertificate = false;
+
+//builder.Services.AddDbContext<CatalogContext>(o =>
+//o.UseSqlServer(providerCs.ToString()));
 // Add services to the container.
 builder.Services.AddRazorPages();
 
