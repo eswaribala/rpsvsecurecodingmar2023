000f767929117a027ea0b913c0833dd6346e2030
eswaribala <parameswaribala@gmail.com>
2023-05-29 19:31:04 +0530

    updated

---
+++ BankingAppv7/BankingAppv7/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankingAppv7.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public const string SessionKeyName = "_Name";
+        public const string SessionKeyStatus = "_Status";
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(SessionKeyName)))
+            {
+                HttpContext.Session.SetString(SessionKeyName, "User");
+                HttpContext.Session.SetString(SessionKeyStatus, "Active");
+            }
+            var name = HttpContext.Session.GetString(SessionKeyName);
+            var status = HttpContext.Session.GetString(SessionKeyStatus);
+
+            _logger.LogInformation("Session Name: {Name}", name);
+            _logger.LogInformation("Session Status: {Status}", status);
+        }
+    }
+}
