5967ff26b3681c5d366bd5b0f407eeb736fcbcb1
eswaribala <parameswaribala@gmail.com>
2023-06-05 19:42:22 +0530

    updated

---
+++ GrpcServiceDemo/GrpcServiceDemo/Services/CustomerService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+
+namespace GrpcServiceDemo.Services
+{
+    public class CustomerService:CustomerProcess.CustomerProcessBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<ShowResponse>AddCustomer(AddRequest addRequest, ServerCallContext context)
+        {
+            return Task.FromResult(new ShowResponse
+            {
+                Message = "Customer Added" + addRequest.Customer.FirstName
+            }); 
+
+        }
+    }
+}
--- GrpcServiceDemo/GrpcServiceDemo/Services/CustomerSrevice.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace GrpcServiceDemo.Services
-{
-    public class CustomerSrevice
-    {
-    }
-}
