25c28d5df69f98ab2e9776f8351520e65ed867fe
eswaribala <parameswaribala@gmail.com>
2023-06-07 19:56:31 +0530

    updated

--- BankingAntiForgeryTokenApp/BankingAntiForgeryTokenApp/Program.cs
+++ BankingAntiForgeryTokenApp/BankingAntiForgeryTokenApp/Program.cs
@@ -12,6 +12,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+//clickjacking
+app.Use(async (context, next) => {
+context.Response.Headers.Add("X-Frame-Options", "DENY"); 
+context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN"); 
+   await next();});
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
