a3382dab4652fde1117ad8ec0540f47e0336fa69
eswaribala <parameswaribala@gmail.com>
2023-06-07 18:51:10 +0530

    updated

---
+++ BankingAntiForgeryTokenApp/BankingAntiForgeryTokenApp/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankingAntiForgeryTokenApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(string userName, string password)
+        {
+            ViewBag.Name = string.Format("Name: {0} {1}", userName, password);
+            return View();
+        }
+    }
+}
